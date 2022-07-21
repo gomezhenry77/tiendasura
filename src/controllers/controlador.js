a0c5d19fa8714fade2a9271fa4f4a12ed7f84b28
gomezhenry77 <gomezhenry77@gmail.com>
2022-07-21 17:42:47 -0500

    se modifica el index y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,10 @@
+//creando variables
+let nombreUsuario="Henry Gómez"
+const EDAD_USUARIO=33
+let estaturaUsuario=1.65
+let aficcionadoCiclismo=true
+
+console.log(nombreUsuario)
+EDAD_USUARIO=20
+console.log(EDAD_USUARIO)
+
